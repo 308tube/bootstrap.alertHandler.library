950d85020849c89e7b7a160af4427f09303784e4
308tube <308tube@gmail.com>
2012-02-24 21:27:18 -0500

    initial project commit

---
+++ WebContent/js/bootstrap.alertHandler.js
@@ -0,0 +1,585 @@
+/*
+ * Alert Library for Bootstrap
+ * version: 0.07.01 (2011-FEB-24)
+ * http://www.308tube.com/bootstrap/
+ * 308tube@gmail.com
+ * 
+ * Requires:
+ * 1. Bootstrap 2.0
+ * 2. jQuery
+ * 3. bootstrap.alertHandler.js
+ *
+ * This document is licensed as free software under the terms of the
+ * MIT License: http://www.opensource.org/licenses/mit-license.php
+ *
+ * "NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK."
+ * 
+ * Helpful reads:
+ * http://en.wikipedia.org/wiki/Fluent_interface#JavaScript
+ * http://stackoverflow.com/questions/5370538/best-way-to-implement-javascript-chaining-in-a-library
+ * http://stackoverflow.com/questions/4142554/calling-function-within-prototype-object
+ */
+
+/**
+ * This is the Msg Code Library
+ * You can add your own messages into this object and
+ * use the key to retrieve the code.
+ * 
+ * Function returns a array of messages to BSMsg.
+ * 
+ * @returns {Array} returns object that contains message strings
+ */
+function _bootstrap_alert_msg_library()
+{
+	//return array of objects
+	return [
+		{
+	       	key: 0, //key is stored as a numeric message
+	       	display_msg: "This is the message displayed to the users",
+	       	sys_msg: "This is the message that will be sent up to the logging service"
+		},
+		{
+	       	key: -1, 
+	       	display_msg: "A negative numeric number signify a error message",
+	       	sys_msg: "All negative numbers are error messages. This allows the developer" +
+	       			"to quickly understand if a message is good or bad."
+		},
+		{
+	       	key: 1, 
+	       	display_msg: "A positive numeric number signify a warning or positive message",
+	       	sys_msg: "All positive numbers are for warning and positive messages. This allows " +
+	       			"the developer to quickly understand if a message is good or bad."
+		},
+		{
+	       	key: 2, 
+	       	display_msg: "Ready",
+	       	sys_msg: "All system report ready"
+		},
+		{
+	       	key: -2, 
+	       	display_msg: "Error",
+	       	sys_msg: "System not ready"
+		},
+		{
+	       	key: 3, 
+	       	display_msg: "Starting",
+	       	sys_msg: "System is starting up, not currently ready"
+		}
+	];
+}
+
+
+/**
+ * Constructor function to create a instance of our function
+ * that will handle the creation of our alert messages.
+ * 
+ * @param elementID is the element ID that this function will write to.
+ * @returns this (pointer)
+ */
+function _BSMsg(elementID) 
+{
+	try
+	{
+		if (!(this instanceof _BSMsg)) {
+			return new _BSMsg(elementID);
+		}
+		
+		this.setElementID(elementID); //valid ID and <div> tag
+		
+		this.msgBlockStyle = false; //Bootstrap style alert-block set to false, not used
+		this.msgColor = "yellow"; //default color if one is not specified
+		this.internalErr = false; //parameter used to check if a error has happened
+		return this;
+	}
+	catch(err)
+	{
+		this.internalErrorHandling(err);
+		return this;
+	}
+}
+_BSMsg.prototype = {
+	/**
+	 * call this method to set the <div> ID element/tag you want to work
+	 * with for your alert box.  This method will validate if the elementID
+	 * parameter is a ID attribute and its a <div> tag.
+	 * 
+	 * @param elementID - ID of <div> tag/element
+	 * @returns {_BSMsg} - this pointer
+	 */
+	setElementID: function(elementID) {
+		try
+		{			
+			/*
+			 * Validates that the elementID is a id attribute
+			 * Currently no other jquery selector will be supported.
+			 */
+			if ( $("#"+elementID).length ) {
+				
+				this.$elementID = $("#"+elementID); //jQuery selector
+				
+				/*
+				 * Per Bootstrap standards 
+				 * http://twitter.github.com/bootstrap/components.html#alerts
+				 * Alerts must be defined in a <div> tag.
+				 */
+				if ( this.$elementID.is("div") == false ) {
+					throw "element_not_a_div";
+				}
+			}
+			else {
+				throw "not_a_ID";
+			}
+			
+			return this;
+		}
+		catch(err)
+		{
+			this.internalErrorHandling(err);
+			return this;
+		}
+	},
+	/**
+	 * call this method to either retrieve the message you want to use
+	 * from the Msg Code Library or you can define your own by passing
+	 * in a object.
+	 * 
+	 * If you are passing in a object display_msg and sys_msg are 
+	 * required keys.
+	 * 
+	 * @param val - number or object
+	 * @returns {_BSMsg} - this pointer
+	 */
+	errMsg: function(val) {
+		try
+		{
+			if (this.internalErr == true) {
+				throw "errMsg.internalError";
+			}
+			
+			if (typeof val === 'number') {
+				this.getErrorCode(val);
+				
+				if (this.internalErr == true) {
+					throw "errMsg.internalError";
+				}
+				
+				if (typeof this.libraryAlertCode === 'undefined'){
+					throw "no_value_found";
+				}
+			}
+			else {
+				if (typeof val === 'object') {
+					
+					var val1 = false;
+					var val2 = false;
+					
+					this.libraryAlertCode = {
+						key: null,
+						display_msg: null,
+						sys_msg: null
+					};
+					
+					if (typeof val.display_msg === 'string'){
+						val1 =  true;
+						this.libraryAlertCode.display_msg = val.display_msg;
+					}
+					
+					if (typeof val.sys_msg === 'string'){
+						val2 =  true;
+						this.libraryAlertCode.sys_msg = val.sys_msg;
+					}
+					
+					if (val1 == false && val2 == false){
+						throw "incorrect_libraryErrCode";
+					}
+				}
+				else {
+					throw "val_not_recongized";
+				}
+			}
+			
+			return this;
+		}
+		catch(err)
+		{
+			this.internalErrorHandling(err);
+			return this;
+		}
+	},
+	/**
+	 * call this method to retrieve the alert message that you
+	 * want to use from the Msg Code Library.
+	 * 
+	 * @param errorNumber - a number used for lookup
+	 * @returns {_BSMsg} - this pointer
+	 */
+	getErrorCode: function(errorNumber) {
+		try
+		{
+			var alert_msg_library = _bootstrap_alert_msg_library();
+			var counter = alert_msg_library.length - 1;			
+			
+			for (var i = 0; i <= counter; i++){
+				if (alert_msg_library[i].key == errorNumber){
+					this.libraryAlertCode = alert_msg_library[i];
+				}
+			}
+			
+			return this;
+		}
+		catch(err)
+		{
+			this.internalErrorHandling(err);
+			return this;
+		}  
+	},
+	/**
+	 * call this method to set the color you want your alert
+	 * box to have.  Only 4 colors are allowed: yellow, green,
+	 * red, blue.
+	 * 
+	 * @param color - yellow, green, red or blue
+	 * @returns {_BSMsg} - this pointer
+	 */
+	setColor: function(color) {
+		try
+		{
+			if (this.internalErr == true) {
+				throw "setColor.internalError";
+			}
+			
+			if (typeof color !== 'string') {
+				throw "not_a_valid_color";
+			}
+			else {
+				
+				var trim_color = jQuery.trim(color);
+				
+				//value must be red, blue, yellow or green
+				if ( trim_color == "red" ||
+					 trim_color == "blue" ||
+					 trim_color == "green" ||
+					 trim_color == "yellow" ) 
+				{	
+					this.msgColor = trim_color;
+				}
+				else 
+				{
+					throw "not_a_valid_color";
+				}
+			}
+			
+			return this;
+		}
+		catch(err)
+		{
+			this.internalErrorHandling(err);
+			return this;
+		}
+	},
+	/**
+	 * call this method to set the amount of time you
+	 * want the alert box to be displayed to the user.
+	 * 1000 = 1 second.
+	 * 
+	 * @param delayTime - number - 1000 = 1 second
+	 * @returns {_BSMsg} - this pointer
+	 */
+	setDisplayTime: function(delayTime) {
+		try
+		{
+			if (this.internalErr == true) {
+				throw "setDisplayTime.internalError";
+			}
+			
+			if (typeof delayTime === 'number') {
+				this.DisplayTimeValue = delayTime;
+			}
+			else {
+				throw "not_a_number";
+			}
+			
+			return this;
+		}
+		catch(err)
+		{
+			this.internalErrorHandling(err);
+			return this;
+		}
+	},
+	/**
+	 * call this method when you are ready to build your
+	 * alert box. The errMsg method MUST have been called
+	 * prior to build.  The time parameter is used to tell
+	 * the method how quickly the alert box will build.
+	 * 
+	 * time is a parameter that can accept a number or string.
+	 * String values can be: "fast" or "slow";
+	 * number values - 1000 = 1 second;
+	 * if you do not pass in a value, then alert box will 
+	 * instantly appear.
+	 * 
+	 * @param time - number or string or "leave empty"
+	 * @returns {_BSMsg} - this pointer
+	 */
+	build: function(time) {
+		try
+		{
+			if (this.internalErr == true) {
+				throw "build.internalError";
+			}
+			
+			var val1 = false;
+			var val2 = false;
+			
+			if (typeof this.libraryAlertCode === 'object'){
+				
+				var display_msg_str = null;
+				var sys_msg_str = null;
+				
+				if (typeof this.libraryAlertCode.display_msg === 'string'){
+					val1 =  true;
+					display_msg_str = this.libraryAlertCode.display_msg;
+					
+				}
+				if (typeof this.libraryAlertCode.sys_msg === 'string'){
+					val2 =  true;
+					sys_msg_str = this.libraryAlertCode.sys_msg;
+				}
+				if (val1 == false && val2 == false){
+					throw "can_not_read_errMsg";
+				}
+				if (val1 == false) {
+					display_msg_str = sys_msg_str;
+				}
+				
+				var $ID = this.$elementID;
+				var added_class = false;
+				var whatColor = this.msgColor;
+				var msg_string = null;
+				var displayTime = null;
+				
+				if (typeof this.DisplayTimeValue === 'number'){
+					displayTime = this.DisplayTimeValue;
+				}
+				
+				if ( $ID.is(":visible") == true ){
+					$ID.hide(); //alert("hide");
+				}
+				
+				if ( $ID.hasClass("alert") == false ){
+					$ID.addClass("alert");
+				}
+				if ( this.msgBlockStyle == true ) {
+					if ( $ID.hasClass("alert-block") == false ){
+						$ID.addClass("alert-block");
+					}
+				}
+				if ( $ID.hasClass("alert-error") == true ){
+					if (whatColor == "red") {
+						added_class = true;
+					}
+					else {
+						$ID.removeClass("alert-error");
+					}
+				}
+				if ( $ID.hasClass("alert-success") == true ){
+					if (whatColor == "green") {
+						added_class = true;
+					}
+					else {
+						$ID.removeClass("alert-success");
+					}
+				}
+				if ( $ID.hasClass("alert-info") == true ){
+					if (whatColor == "blue") {
+						added_class = true;
+					}
+					else {
+						$ID.removeClass("alert-info");
+					}
+				}
+				if ( added_class == false ) {
+					
+					switch(whatColor)
+					{
+					case "red":
+						$ID.addClass("alert-error");
+						break;
+					case "green":
+						$ID.addClass("alert-success");
+						break;
+					case "blue":
+						$ID.addClass("alert-info");
+						break;
+					}
+				}
+				
+				//http://stackoverflow.com/questions/1349404/generate-a-string-of-5-random-characters-in-javascript
+				var makeid = function(){
+					var text = "";
+				    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+				    for( var i=0; i < 10; i++ )
+				        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+				    return text;
+				};
+				var random_id = makeid(); //alert(random_id);
+				
+				msg_string = '<a class="close" id="'+random_id+'">x</a>'+display_msg_str;
+				
+				if (typeof time === 'number' || time == "slow" || time == "fast"){
+					$ID.html(msg_string).show(time);
+				}
+				else {
+					$ID.html(msg_string).show();
+				}
+				
+				if (displayTime != null){
+					if (displayTime != 0){
+						window.setTimeout(function(){
+							$ID.hide();
+						}, displayTime);
+					}					
+				}
+				
+				//http://stackoverflow.com/questions/1386942/jquery-unbinding-is-it-necessary-replacing-elements
+				$("#"+random_id).click(function(){
+					$ID.hide();
+				});
+			}
+			else {
+				throw "libraryErrCode_not_defined";
+			}
+			
+			return this;
+		}
+		catch(err)
+		{
+			this.internalErrorHandling(err);
+			return this;
+		}
+	},
+	/**
+	 * this method is used internally to report errors. Developer
+	 * should never have to call this method
+	 * 
+	 * @param errCode - string or object
+	 * @returns {_BSMsg} - this pointer
+	 */
+	internalErrorHandling: function(errCode) {
+		try
+		{
+			this.internalErr = true;
+			var hasConsole = null;
+			
+			if (typeof console === 'undefined') { hasConsole = false; }
+			else { hasConsole = true; }
+			
+			var build_error = function(msg, bConsole) {
+				if(bConsole == false) { alert(msg); }
+				else { console.error(msg); }
+			};
+			
+			switch(errCode)
+			{
+			case "not_a_ID":
+				build_error("The parameter is not a valid element ID attribute", hasConsole);
+				break;
+			case "element_not_a_div":
+				build_error("Per Bootstrap standards, Alerts must be defined in a div tag", hasConsole);
+				break;
+			case "errMsg.internalError":
+				build_error("internalError, errMsg function did not execute", hasConsole);
+				break;
+			case "setColor.internalError":
+				build_error("internalError, setColor function did not execute", hasConsole);
+				break;
+			case "setDisplayTime.internalError":
+				build_error("internalError, setDisplayTime function did not execute", hasConsole);
+				break;
+			case "build.internalError":
+				build_error("internalError, build function did not execute", hasConsole);
+				break;
+			case "val_not_recongized":
+				build_error("The parameter is not a number or object", hasConsole);
+				break;
+			case "no_value_found":
+				build_error("Error Code not found in system library", hasConsole);
+				break;
+			case "incorrect_libraryErrCode":
+				build_error("Error reading your custom errMsg parameter", hasConsole);
+				break;
+			case "libraryErrCode_not_defined":
+				build_error("Error, no errMsg has been defined", hasConsole);
+				break;
+			case "can_not_read_errMsg":
+				build_error("Error reading errMsg parameter", hasConsole);
+				break;
+			case "not_a_valid_color":
+				build_error("There must be a string parameter. Values can be red or blue or yellow or green (case sensitive)", hasConsole);
+				break;
+			case "not_a_number":
+				build_error("Parameter must be a number", hasConsole);
+				break;
+			default:
+				var default_error = errCode.name + " " + errCode.message;
+				build_error(default_error, hasConsole);
+			}
+			
+			return this;
+		}
+		catch(err)
+		{
+			this.internalErr = true;
+			alert(err.name + " " + err.message);
+			return this;
+		}
+	}
+}; 
+
+
+/*
+template: function(val) { 
+	try
+	{
+		if (this.internalErr == true) {
+			throw "_method_.internalError";
+		}
+		
+		return this;
+	}
+	catch(err)
+	{
+		this.internalErrorHandling(err);
+		return this;
+	}
+}
+*/
+
+/*
+if(typeof this.libraryAlertCode === 'object'){
+	var errObj = this.libraryAlertCode;
+	if(errObj.key == errorNumber){
+		//alert("same error code");
+		return this;
+	}
+}
+*/
+
+/*
+ * http://stackoverflow.com/questions/1386942/jquery-unbinding-is-it-necessary-replacing-elements
+ * If the answer is true, then i dont need to unbind the previous click listener
+ 
+var getRandom_id = $ID.attr("random_id"); //alert(getRandom_id);
+
+if(getRandom_id !== undefined){
+	//alert( $("#"+getRandom_id).size() );
+	$("#"+getRandom_id).unbind('click');
+}
+
+$ID.unbind('click'); //unbind all click events on div
+
+//$ID.attr("random_id", random_id);
+ */
---
+++ WebContent/js/main.js
@@ -0,0 +1,155 @@
+
+$(document).ready(function(){
+	
+	$("#example1_btn").click(function(){
+		_BSMsg("example1").errMsg(0).build();
+	});
+	
+	$("#example2a_btn").click(function(){
+		_BSMsg("example2a").errMsg(-1).build("slow");
+	});
+	$("#example2b_btn").click(function(){
+		_BSMsg("example2b").errMsg(1).build(3000);
+	});
+	
+	$("#example3a_btn").click(function(){
+		_BSMsg("example3a").errMsg({
+			display_msg: "This message will be displayed to the user",
+			sys_msg: "This is the geek message that will be logged"
+		})
+		.build("fast");
+	});
+	$("#example3b_btn").click(function(){
+		_BSMsg("example3b").errMsg({
+			display_msg: "How now brown cow",
+			sys_msg: "var cow=brown, error cow is not brown"
+		})
+		.build(1000);
+	});
+	
+	$("#example4a_btn").click(function(){
+		_BSMsg("example4a").errMsg(0).setColor("blue").build("slow");
+	});
+	$("#example4b_btn").click(function(){
+		_BSMsg("example4b").setColor("red").errMsg(-1).build("fast");
+	});
+	$("#example4c_btn").click(function(){
+		_BSMsg("example4c").errMsg(1).setColor("green").build("slow");
+	});
+	$("#example4d_btn").click(function(){
+		_BSMsg("example4d").errMsg({
+			display_msg: "How now brown cow",
+			sys_msg: "var cow=brown, error cow is not brown"
+		})
+		.setColor("yellow")
+		.build("fast");
+	});
+	
+	
+	$("#example5a_btn").click(function(){
+		_BSMsg("example5a").setDisplayTime(0).errMsg(0).setColor("blue").build("fast");
+		_BSMsg("example5b").setDisplayTime(3000).setColor("red").errMsg(-1).build("fast");
+		_BSMsg("example5c").setDisplayTime(2000).errMsg(1).setColor("green").build("fast");
+		_BSMsg("example5d").setDisplayTime(1000)
+		.errMsg({
+			display_msg: "How now brown cow",
+			sys_msg: "var cow=brown, error cow is not brown"
+		})
+		.setColor("yellow")
+		.build("fast");
+	});
+	
+	$("#example6a_btn").click(function(){
+		_BSMsg("example6a").setDisplayTime(4000).errMsg(0).setColor("blue").build("fast")
+			.setElementID("example6b").setDisplayTime(3000).setColor("red").errMsg(-1).build("fast")
+			.setElementID("example6c").setDisplayTime(2000).errMsg(1).setColor("green").build("fast")
+			.setElementID("example6d").setDisplayTime(1000)
+				.errMsg({
+						display_msg: "How now brown cow",
+						sys_msg: "var cow=brown, error cow is not brown"
+				})
+				.setColor("yellow")
+				.build("fast");
+	});
+	
+	$("#example7a_btn").click(function(){
+		
+		//code here to check on pos1 status
+		var status_check = _BSMsg("pos1").setDisplayTime(5000).errMsg(2).setColor("green").build("fast");
+		
+		//code here to check on pos2 status
+		status_check.setElementID("pos2").build("fast");
+		
+		//code here to check on pos3 status
+		status_check.setElementID("pos3").errMsg(3).setDisplayTime(0).setColor("yellow").build("fast");
+		
+		//more code, error found
+		status_check.setElementID("pos4").errMsg(-2).setColor("red").build("fast");
+		
+		//more code, system ready
+		status_check.setElementID("pos5").setDisplayTime(5000).errMsg(2).setColor("green").build("fast");
+		
+		//more code, system ready
+		status_check.setElementID("pos6").build("fast"); //reuses values
+		
+		//more code, system ready
+		status_check.setElementID("pos7").build("fast"); //reuses values
+		
+		//more code, system ready
+		status_check.setElementID("pos8").build("fast"); //reuses values
+		
+		//more code, found error
+		status_check.setElementID("pos9").errMsg(-2).setColor("red").setDisplayTime(0).build("fast");
+		
+		//more code, system not ready
+		status_check.setElementID("pos10").errMsg(3).setColor("yellow").build("fast");
+		
+		//more code, found error
+		status_check.setElementID("pos11").errMsg(-2).setColor("red").build("fast");
+		
+		//more code, system ready
+		status_check.setElementID("pos12").errMsg(2).setColor("green").setDisplayTime(5000).build("fast");
+	});
+	
+	$("#example8a_btn").click(function(){
+		
+		//code here to check on pos1 status
+		var green_status = _BSMsg("pos1b").setDisplayTime(5000).errMsg(2).setColor("green").build("fast"); //build green status
+		
+		//code here to check on pos2 status
+		green_status.setElementID("pos2b").build("fast"); //reuses green values
+		
+		//code here to check on pos3 status
+		var yellow_status = _BSMsg("pos3b").errMsg(3).setDisplayTime(0).setColor("yellow").build("fast"); //build yellow status
+		
+		//more code, error found
+		var red_status = _BSMsg("pos4b").errMsg(-2).setColor("red").build("fast"); //build red status
+		
+		//more code, system ready
+		green_status.setElementID("pos5b").build("fast"); //reuses green values
+		
+		//more code, system ready
+		green_status.setElementID("pos6b").build("fast"); //reuses green values
+		
+		//more code, system ready
+		green_status.setElementID("pos7b").build("fast"); //reuses green values
+		
+		//more code, system ready
+		green_status.setElementID("pos8b").build("fast"); //reuses green values
+		
+		//more code, found error
+		red_status.setElementID("pos9b").build("fast"); //reuses red values
+		
+		//more code, system not ready
+		yellow_status.setElementID("pos10b").build("fast"); //reuses yellow values
+		
+		//more code, found error
+		red_status.setElementID("pos11b").build("fast"); //reuses red values
+		
+		//more code, system ready
+		green_status.setElementID("pos12b").build("fast"); //reuses green values
+	});
+	
+	
+});
+
